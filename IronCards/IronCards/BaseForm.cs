c8fbc838dafd599189add2d811d78527ac09d394
jay katie martin <jaykatiemartin@outlook.com>
2019-06-02 20:45:26 +0100

    trying a diffrent UI kit -> https://thielj.github.io/MetroFramework/#GettingStarted

--- IronCards/IronCards/BaseForm.cs
+++ IronCards/IronCards/BaseForm.cs
@@ -1,19 +1,23 @@
 ﻿using System.Windows.Forms;
 using MaterialSkin;
 using MaterialSkin.Controls;
+using MetroFramework;
 
 namespace IronCards
 {
-    public class BaseForm: MaterialForm
+    public class BaseForm: MetroFramework.Forms.MetroForm
     {
         public BaseForm()
         {
-            base.FormBorderStyle = FormBorderStyle.Sizable;
+          /**  base.FormBorderStyle = FormBorderStyle.Sizable;
             
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.Indigo400, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        **/
+          base.Theme = MetroThemeStyle.Dark;
+
 
         }
 
